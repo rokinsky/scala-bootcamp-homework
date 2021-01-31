add3adab0f20c236673b14b67efb51050dd10044
Aleksander Sorokin <ankezy@gmail.com>
2021-01-31 19:28:13 +0300

    replace hardcoded it with behavior in basics tests

--- src/test/scala/com/evolutiongaming/bootcamp/basics/BasicsSpec.scala
+++ src/test/scala/com/evolutiongaming/bootcamp/basics/BasicsSpec.scala
@@ -14,7 +14,9 @@ class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
   val squareBoundedIntegers: Gen[Int] = boundIntegers(sqrt(Int.MaxValue).toInt)
   val cubeBoundedIntegers: Gen[Int] = boundIntegers(cbrt(Int.MaxValue).toInt)
 
-  "gcd" should "be correct for random generated values" in {
+  behavior of "gcd"
+
+  it should "be correct for random generated values" in {
     forAll (squareBoundedIntegers, squareBoundedIntegers) {
       (a: Int, b: Int) => {
         gcd(a, b) shouldEqual BigInt(a).gcd(BigInt(b))
@@ -22,7 +24,7 @@ class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
     }
   }
 
-  "gcd" should "follow the commutative law" in {
+  it should "follow the commutative law" in {
     forAll (squareBoundedIntegers, squareBoundedIntegers) {
       (a: Int, b: Int) => {
         gcd(a, b) shouldEqual gcd(b, a)
@@ -30,7 +32,7 @@ class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
     }
   }
 
-  "gcd" should "follow the associative law" in {
+  it should "follow the associative law" in {
     forAll (squareBoundedIntegers, squareBoundedIntegers, squareBoundedIntegers) {
       (a: Int, b: Int, c: Int) => {
         gcd(a, gcd(b, c)) shouldEqual gcd(gcd(a, b), c)
@@ -38,7 +40,7 @@ class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
     }
   }
 
-  "gcd" should "follow the absorption law" in {
+  it should "follow the absorption law" in {
     forAll (squareBoundedIntegers, squareBoundedIntegers) {
       (a: Int, b: Int) => {
         gcd(a, lcm(a, b)) shouldEqual a.abs
@@ -46,7 +48,7 @@ class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
     }
   }
 
-  "gcd" should "follow the idempotent law" in {
+  it should "follow the idempotent law" in {
     forAll (squareBoundedIntegers) {
       (a: Int) => {
         gcd(a, a) shouldEqual a.abs
@@ -54,7 +56,7 @@ class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
     }
   }
 
-  "gcd" should "be distributed over lcm" in {
+  it should "be distributed over lcm" in {
     forAll (squareBoundedIntegers, squareBoundedIntegers, squareBoundedIntegers) {
       (a: Int, b: Int, c: Int) => {
         gcd(a, lcm(b, c)) shouldEqual lcm(gcd(a, b), gcd(a, c))
@@ -62,7 +64,9 @@ class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
     }
   }
 
-  "lcm" should "be correct for non-zero random generated values" in {
+  behavior of "lcm"
+
+  it should "be correct for non-zero random generated values" in {
     forAll (squareBoundedIntegers, squareBoundedIntegers) {
       (a: Int, b: Int) => {
         whenever(a != 0 && b != 0) {
@@ -72,11 +76,11 @@ class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
     }
   }
 
-  "lcm" should "be correct for zero values" in {
+  it should "be correct for zero values" in {
     lcm(0, 0) shouldEqual 0
   }
 
-  "lcm" should "follow the commutative law" in {
+  it should "follow the commutative law" in {
     forAll (squareBoundedIntegers, squareBoundedIntegers) {
       (a: Int, b: Int) => {
         lcm(a, b) shouldEqual lcm(b, a)
@@ -84,7 +88,7 @@ class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
     }
   }
 
-  "lcm" should "follow the associative law" in {
+  it should "follow the associative law" in {
     forAll (cubeBoundedIntegers, cubeBoundedIntegers, cubeBoundedIntegers) {
       (a: Int, b: Int, c: Int) => {
         lcm(a, lcm(b, c)) shouldEqual lcm(lcm(a, b), c)
@@ -92,7 +96,7 @@ class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
     }
   }
 
-  "lcm" should "follow the absorption law" in {
+  it should "follow the absorption law" in {
     forAll (squareBoundedIntegers, squareBoundedIntegers) {
       (a: Int, b: Int) => {
         lcm(a, gcd(a, b)) shouldEqual a.abs
@@ -100,7 +104,7 @@ class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
     }
   }
 
-  "lcm" should "follow the idempotent law" in {
+  it should "follow the idempotent law" in {
     forAll (squareBoundedIntegers) {
       (a: Int) => {
         lcm(a, a) shouldEqual a.abs
@@ -108,7 +112,7 @@ class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
     }
   }
 
-  "lcm" should "be distributed over gcd" in {
+  it should "be distributed over gcd" in {
     forAll (squareBoundedIntegers, squareBoundedIntegers, squareBoundedIntegers) {
       (a: Int, b: Int, c: Int) => {
         lcm(a, gcd(b, c)) shouldEqual gcd(lcm(a, b), lcm(a, c))
