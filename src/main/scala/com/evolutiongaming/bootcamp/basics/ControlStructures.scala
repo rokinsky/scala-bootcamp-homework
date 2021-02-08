b7d433c4f5efced29dab0dcf5b06026eb0cd799e
Aleksander Sorokin <ankezy@gmail.com>
2021-02-08 21:45:58 +0300

    remove unnecessary private modifier in command constructors

--- src/main/scala/com/evolutiongaming/bootcamp/basics/ControlStructures.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/basics/ControlStructures.scala
@@ -50,11 +50,11 @@ object ControlStructures {
 
   sealed trait Command
   object Command {
-    final case class Divide private(dividend: Double, divisor: Double) extends Command
-    final case class Sum private(numbers: List[Double]) extends Command
-    final case class Average private(numbers: List[Double]) extends Command
-    final case class Min private(numbers: List[Double]) extends Command
-    final case class Max private(numbers: List[Double]) extends Command
+    final case class Divide(dividend: Double, divisor: Double) extends Command
+    final case class Sum(numbers: List[Double]) extends Command
+    final case class Average(numbers: List[Double]) extends Command
+    final case class Min(numbers: List[Double]) extends Command
+    final case class Max(numbers: List[Double]) extends Command
 
     import NumberParser._
 
