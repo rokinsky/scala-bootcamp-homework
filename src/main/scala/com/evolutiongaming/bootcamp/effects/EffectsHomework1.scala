2212dec7bee96471e1b9ca3dd355f9643625ccbc
Aleksander Sorokin <ankezy@gmail.com>
2021-03-31 16:24:41 +0300

    add io-effects implementations with tests

--- src/main/scala/com/evolutiongaming/bootcamp/effects/EffectsHomework1.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/effects/EffectsHomework1.scala
@@ -1,6 +1,6 @@
 package com.evolutiongaming.bootcamp.effects
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
 
 /*
@@ -27,35 +27,69 @@ import scala.util.Try
  * Ask questions in the bootcamp chat if stuck on this task.
  */
 object EffectsHomework1 {
-  final class IO[A] {
-    def map[B](f:       A => B):     IO[B] = ???
-    def flatMap[B](f:   A => IO[B]): IO[B] = ???
-    def *>[B](another:  IO[B]):      IO[B] = ???
-    def as[B](newValue: => B):       IO[B] = ???
-    def void:    IO[Unit]                 = ???
-    def attempt: IO[Either[Throwable, A]] = ???
-    def option:  IO[Option[A]]            = ???
-    def handleErrorWith[AA >: A](f: Throwable => IO[AA]): IO[AA] = ???
-    def redeem[B](recover:          Throwable => B, map: A => B): IO[B] = ???
-    def redeemWith[B](recover:      Throwable => IO[B], bind: A => IO[B]): IO[B] = ???
-    def unsafeRunSync():  A         = ???
-    def unsafeToFuture(): Future[A] = ???
+  final class IO[A](private val run: () => A) {
+    def map[B](f: A => B): IO[B] = IO(f(run()))
+
+    def flatMap[B](f: A => IO[B]): IO[B] = IO(f(run()).run())
+
+    def *>[B](another: IO[B]): IO[B] = flatMap(_ => another)
+
+    def as[B](newValue: => B): IO[B] = map(_ => newValue)
+
+    def void: IO[Unit] = map(_ => ())
+
+    def attempt: IO[Either[Throwable, A]] = IO(Try(run()).toEither)
+
+    def option: IO[Option[A]] = attempt.map(_.toOption)
+
+    def handleErrorWith[AA >: A](f: Throwable => IO[AA]): IO[AA] =
+      Try(run()).fold(f, IO(_))
+
+    def redeem[B](recover: Throwable => B, map: A => B): IO[B] =
+      attempt.map(_.fold(recover, map))
+
+    def redeemWith[B](recover: Throwable => IO[B], bind: A => IO[B]): IO[B] =
+      attempt.flatMap(_.fold(recover, bind))
+
+    def unsafeRunSync(): A = run()
+
+    def unsafeToFuture()(implicit ec: ExecutionContext): Future[A] = Future(run())
   }
 
   object IO {
-    def apply[A](body:        => A):     IO[A] = ???
-    def suspend[A](thunk:     => IO[A]): IO[A] = ???
-    def delay[A](body:        => A):     IO[A] = ???
-    def pure[A](a:            A):        IO[A] = ???
-    def fromEither[A](e:      Either[Throwable, A]): IO[A] = ???
-    def fromOption[A](option: Option[A])(orElse: => Throwable): IO[A] = ???
-    def fromTry[A](t:         Try[A]):   IO[A] = ???
-    def none[A]: IO[Option[A]] = ???
-    def raiseError[A](e:  Throwable): IO[A] = ???
-    def raiseUnless(cond: Boolean)(e: => Throwable): IO[Unit] = ???
-    def raiseWhen(cond:   Boolean)(e:      => Throwable): IO[Unit] = ???
-    def unlessA(cond:     Boolean)(action: => IO[Unit]):  IO[Unit] = ???
-    def whenA(cond:       Boolean)(action: => IO[Unit]):  IO[Unit] = ???
-    val unit: IO[Unit] = ???
+    def apply[A](body: => A): IO[A] = delay(body)
+
+    def suspend[A](thunk: => IO[A]): IO[A] = thunk
+
+    def delay[A](body: => A): IO[A] = new IO(() => body)
+
+    def pure[A](a: A): IO[A] = IO(a)
+
+    def fromEither[A](e: Either[Throwable, A]): IO[A] =
+      e.fold(raiseError, pure)
+
+    def fromOption[A](option: Option[A])(orElse: => Throwable): IO[A] =
+      option.fold(raiseError[A](orElse))(pure)
+
+    def fromTry[A](t: Try[A]): IO[A] =
+      t.fold(raiseError, pure)
+
+    def none[A]: IO[Option[A]] = pure(None)
+
+    def raiseError[A](e: Throwable): IO[A] = IO(throw e)
+
+    def raiseUnless(cond: Boolean)(e: => Throwable): IO[Unit] =
+      raiseWhen(!cond)(e)
+
+    def raiseWhen(cond: Boolean)(e: => Throwable): IO[Unit] =
+      whenA(cond)(raiseError(e))
+
+    def unlessA(cond: Boolean)(action: => IO[Unit]): IO[Unit] =
+      whenA(!cond)(action)
+
+    def whenA(cond: Boolean)(action: => IO[Unit]): IO[Unit] =
+      if (cond) action else unit
+
+    val unit: IO[Unit] = pure(())
   }
 }
---
+++ src/test/scala/com/evolutiongaming/bootcamp/effects/EffectsHomework1Spec.scala
@@ -0,0 +1,332 @@
+package com.evolutiongaming.bootcamp.effects
+
+import com.evolutiongaming.bootcamp.effects.EffectsHomework1._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks.forAll
+
+import java.io.ByteArrayOutputStream
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
+import scala.util.{Failure, Success, Try}
+
+class EffectsHomework1Spec extends AnyFlatSpec with Matchers {
+  "map" should "transform values" in {
+    forAll { (x: String, y: String) =>
+      {
+        val a = IO(x)
+        val c = a.map(_ => y)
+        c.unsafeRunSync() shouldEqual y
+      }
+    }
+  }
+
+  "flatMap" should "work correctly" in {
+    forAll { (x: String, y: String) =>
+      {
+        val a = IO(x)
+        val b = IO(y)
+        val c = a.flatMap(_ => b)
+        c.unsafeRunSync() shouldEqual y
+      }
+    }
+  }
+
+  "flatMap" should "sequence the side effects" in {
+    forAll { (x: String, y: String) =>
+      val baos = new ByteArrayOutputStream
+      Console.withOut(baos) {
+        val out = for {
+          _ <- IO(print(x))
+          _ <- IO(print(y))
+        } yield ()
+        baos.toString shouldEqual ""
+        out.unsafeRunSync()
+        baos.toString shouldEqual x + y
+      }
+    }
+  }
+
+  "*>" should "ignore the first action" in {
+    forAll { (x: String, y: String) =>
+      val a = IO(x)
+      val b = IO(y)
+      val c = a *> b
+      c.unsafeRunSync() shouldEqual y
+    }
+  }
+
+  "*>" should "not run if source fails" in {
+    forAll { (x: String, ex: Exception) =>
+      val a = IO.raiseError(ex)
+      val b = IO(x)
+      val c = a *> b
+      an[Exception] should be thrownBy c.unsafeRunSync()
+    }
+  }
+
+  "as" should "replace the result of IO" in {
+    forAll { (x: String, y: String) =>
+      val a = IO(x)
+      val b = IO(y)
+      val c = a as b
+      c.unsafeRunSync() shouldEqual b
+    }
+  }
+
+  "void" should "return unit" in {
+    forAll { (x: String) =>
+      val input = IO(x).void
+      input.unsafeRunSync() shouldEqual ()
+    }
+  }
+
+  "attempt" should "materialize an exception" in {
+    forAll { (ex: Exception) =>
+      val b = IO.raiseError(ex).attempt
+      b.unsafeRunSync() shouldEqual Left(ex)
+    }
+  }
+
+  it should "work with valid value" in {
+    forAll { (x: Int) =>
+      val b = IO(x).attempt
+      b.unsafeRunSync() shouldEqual Right(x)
+    }
+  }
+
+  "option" should "work with valid data" in {
+    forAll { (x: Int) =>
+      val input = IO(x).option
+      input.unsafeRunSync() shouldEqual Some(x)
+    }
+  }
+
+  it should "return None when data is invalid" in {
+    val input = IO("not-int".toInt).option
+    input.unsafeRunSync() shouldEqual None
+  }
+
+  "handleErrorWith" should "handle errors" in {
+    forAll { (x: Int) =>
+      val input = IO(x / 0).handleErrorWith(_ => IO(-1))
+      input.unsafeRunSync() shouldEqual -1
+    }
+  }
+
+  it should "handle valid values" in {
+    forAll { (x: Int) =>
+      val input = IO(x / 1).handleErrorWith(_ => IO(-1))
+      input.unsafeRunSync() shouldEqual x
+    }
+  }
+
+  "redeem" should "handle errors" in {
+    forAll { (x: Int) =>
+      val input = IO(x / 0).redeem(_ => None, Some(_))
+      input.unsafeRunSync() shouldEqual None
+    }
+  }
+
+  it should "handle valid values" in {
+    forAll { (x: Int) =>
+      val input = IO(x / 1).redeem(_ => None, Some(_))
+      input.unsafeRunSync() shouldEqual Some(x)
+    }
+  }
+
+  "redeemWith" should "handle errors" in {
+    forAll { (x: Int) =>
+      val input = IO(x / 0).redeemWith(_ => IO[Option[Int]](None), v => IO[Option[Int]](Some(v)))
+      input.unsafeRunSync() shouldEqual None
+    }
+  }
+
+  it should "handle valid values" in {
+    forAll { (x: Int) =>
+      val input = IO(x / 1).redeemWith(_ => IO[Option[Int]](None), v => IO[Option[Int]](Some(v)))
+      input.unsafeRunSync() shouldEqual Some(x)
+    }
+  }
+
+  "unsafeToFuture" should "handle valid values" in {
+    implicit val ec: ExecutionContextExecutor = ExecutionContext.global
+    forAll { (x: Int, y: Int) =>
+      IO(x + y).unsafeToFuture().onComplete {
+        case Success(value)     => value shouldEqual x + y
+        case Failure(exception) => fail(s"${x + y} expected, but $exception found")
+      }
+    }
+  }
+
+  it should "handle errors" in {
+    implicit val ec: ExecutionContextExecutor = ExecutionContext.global
+    forAll { (ex: Exception) =>
+      IO.raiseError(ex).unsafeToFuture().onComplete {
+        case Success(value)     => fail(s"$ex expected, but $value found")
+        case Failure(exception) => exception shouldBe ex
+      }
+    }
+  }
+
+  "suspend" should "suspend the side effects" in {
+    forAll { (x: String) =>
+      val baos = new ByteArrayOutputStream
+      Console.withOut(baos) {
+        val input = IO.suspend { IO(print(x)) }
+        baos.toString shouldEqual ""
+        input.unsafeRunSync()
+        baos.toString shouldEqual x
+      }
+    }
+  }
+
+  "delay" should "suspend the side effects" in {
+    forAll { (x: String) =>
+      val baos = new ByteArrayOutputStream
+      Console.withOut(baos) {
+        val input = IO.delay { print(x) }
+        baos.toString shouldEqual ""
+        input.unsafeRunSync()
+        baos.toString shouldEqual x
+      }
+    }
+  }
+
+  "pure" should "suspend pure values" in {
+    forAll { (x: Int) =>
+      IO.pure(x).unsafeRunSync() shouldEqual x
+    }
+  }
+
+  "fromEither" should "convert from Right" in {
+    forAll { (x: Int) =>
+      val input = IO.fromEither(Right(x)).unsafeRunSync()
+      input shouldEqual x
+    }
+  }
+
+  it should "convert from Left" in {
+    forAll { (ex: Exception) =>
+      val input = IO.fromEither(Left(ex))
+      a[Exception] should be thrownBy input.unsafeRunSync()
+    }
+  }
+
+  "fromOption" should "convert from Some" in {
+    forAll { (x: Int) =>
+      val ex    = new NumberFormatException
+      val input = IO.fromOption(Some(x))(ex).unsafeRunSync()
+      input shouldEqual x
+    }
+  }
+
+  it should "throw exceptions in case of None" in {
+    forAll { (ex: Exception) =>
+      val input = IO.fromOption(None)(ex)
+      a[Exception] should be thrownBy input.unsafeRunSync()
+    }
+  }
+
+  "fromTry" should "convert from Success" in {
+    forAll { (x: Int) =>
+      val input = IO.fromTry(Try(x / 1)).unsafeRunSync()
+      input shouldEqual x
+    }
+  }
+
+  it should "throw exceptions in case of Failure" in {
+    forAll { (x: Int) =>
+      val input = IO.fromTry(Try(x / 0))
+      a[ArithmeticException] should be thrownBy input.unsafeRunSync()
+    }
+  }
+
+  "none" should "return None" in {
+    val input = IO.none
+    input.unsafeRunSync() shouldEqual None
+  }
+
+  "raiseError" should "throw exceptions" in {
+    forAll { (ex: Exception) =>
+      val b = IO.raiseError(ex)
+      a[Exception] should be thrownBy b.unsafeRunSync()
+    }
+  }
+
+  "raiseUnless" should "handle true condition" in {
+    forAll { (ex: Exception) =>
+      val input = IO.raiseUnless(cond = true)(ex)
+      input.unsafeRunSync() shouldEqual ()
+    }
+  }
+
+  it should "handle false condition" in {
+    forAll { (ex: Exception) =>
+      val input = IO.raiseUnless(cond = false)(ex)
+      a[Exception] should be thrownBy input.unsafeRunSync()
+    }
+  }
+
+  "raiseWhen" should "handle true condition" in {
+    forAll { (ex: Exception) =>
+      val input = IO.raiseWhen(cond = true)(ex)
+      a[Exception] should be thrownBy input.unsafeRunSync()
+    }
+  }
+
+  it should "handle false condition" in {
+    forAll { (ex: Exception) =>
+      val input = IO.raiseWhen(cond = false)(ex)
+      input.unsafeRunSync() shouldEqual ()
+    }
+  }
+
+  "unlessA" should "handle true condition" in {
+    forAll { (x: String) =>
+      {
+        val baos = new ByteArrayOutputStream()
+        Console.withOut(baos) {
+          val input = IO.unlessA(cond = true) { IO(print(x)) }
+          baos.toString shouldEqual ""
+          input.unsafeRunSync() shouldEqual ()
+        }
+      }
+    }
+  }
+
+  it should "handle false condition" in {
+    forAll { (x: String) =>
+      val baos = new ByteArrayOutputStream()
+      Console.withOut(baos) {
+        IO.unlessA(cond = false) {
+          IO(print(x))
+        }.unsafeRunSync()
+        baos.toString shouldEqual x
+      }
+    }
+  }
+
+  "whenA" should "handle true condition" in {
+    forAll { (x: String) =>
+      val baos = new ByteArrayOutputStream()
+      Console.withOut(baos) {
+        val input = IO.unlessA(cond = true) {
+          IO(print(x))
+        }
+        input.unsafeRunSync() shouldEqual ()
+      }
+    }
+  }
+
+  it should "handle false condition" in {
+    forAll { (x: String) =>
+      val baos = new ByteArrayOutputStream()
+      Console.withOut(baos) {
+        IO.unlessA(cond = false) {
+          IO(print(x))
+        }.unsafeRunSync()
+        baos.toString shouldEqual x
+      }
+    }
+  }
+}
