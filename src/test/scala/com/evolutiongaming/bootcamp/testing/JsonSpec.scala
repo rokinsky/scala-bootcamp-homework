f28c95a4de08131cb8c857981d22dd062fd30cb5
Aleksander Sorokin <ankezy@gmail.com>
2021-04-07 13:39:38 +0300

    add breadth parameter

--- src/test/scala/com/evolutiongaming/bootcamp/testing/JsonSpec.scala
+++ src/test/scala/com/evolutiongaming/bootcamp/testing/JsonSpec.scala
@@ -1,39 +1,39 @@
 package com.evolutiongaming.bootcamp.testing
 
 import org.scalacheck.Gen
+import org.scalacheck.Test.Parameters
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
-import org.scalacheck.Test.Parameters
 
 class JsonSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks with Matchers {
   import Json._
 
   implicit val params: Parameters = Parameters.default.withMinSuccessfulTests(1000)
 
-  def tupleGen[K, V](firstGen: Gen[K], secondGen: Gen[V]): Gen[(K, V)] =
-    for {
-      first  <- firstGen
-      second <- secondGen
-    } yield (first, second)
+  def tupleGen[K, V](firstGen: Gen[K], secondGen: Gen[V]): Gen[(K, V)] = for {
+    first  <- firstGen
+    second <- secondGen
+  } yield (first, second)
 
-  def jsonGen: Gen[Json] = Gen.sized(jsonGen)
+  def jsonGen: Gen[Json] = Gen.sized(depth => jsonGen(depth, 8))
 
-  def jsonGen(maxDepth: Int): Gen[Json] = maxDepth match {
-    case 0 => jsonPrimitiveGen
-    case n => Gen.oneOf(jsonPrimitiveGen, jsonComplexGen(n))
+  def jsonGen(maxDepth: Int, maxBreadth: Int): Gen[Json] = (maxDepth, maxBreadth) match {
+    case (0, _) => jsonPrimitiveGen
+    case (d, b) => Gen.oneOf(jsonPrimitiveGen, jsonComplexGen(d, b))
   }
 
-  def jsonComplexGen(maxDepth: Int): Gen[Json] = for {
-    depth <- Gen.choose(0, maxDepth - 1)
-    json  <- Gen.oneOf(jsonArrayGen(depth), jsonObjectGen(depth))
+  def jsonComplexGen(maxDepth: Int, maxBreadth: Int): Gen[Json] = for {
+    depth   <- Gen.choose(0, maxDepth - 1)
+    breadth <- Gen.choose(0, maxBreadth)
+    json    <- Gen.oneOf(jsonArrayGen(depth, breadth), jsonObjectGen(depth, breadth))
   } yield json
 
-  def jsonObjectGen(maxDepth: Int): Gen[JObject] =
-    Gen.mapOfN(4, tupleGen(Gen.alphaNumStr, jsonGen(maxDepth))).map(JObject)
+  def jsonObjectGen(depth: Int, breadth: Int): Gen[JObject] =
+    Gen.mapOfN(breadth, tupleGen(Gen.alphaNumStr, jsonGen(depth, breadth))).map(JObject)
 
-  def jsonArrayGen(maxDepth: Int): Gen[JArray] =
-    Gen.containerOfN[Vector, Json](4, jsonGen(maxDepth)).map(JArray)
+  def jsonArrayGen(depth: Int, breadth: Int): Gen[JArray] =
+    Gen.containerOfN[Vector, Json](breadth, jsonGen(depth, breadth)).map(JArray)
 
   def jsonPrimitiveGen: Gen[Json] =
     Gen.oneOf(Gen.const(JNull), jsonBooleanGen, jsonNumberGen, jsonStringGen)
